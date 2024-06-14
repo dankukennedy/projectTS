7ae65bc594e88984bca41d0bac8fabfe308eb940
dankukennedy <dankukennedy@email.com>
2024-06-14 13:59:28 +0000

    creating a new ts project with tailwine css

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl text-blue-500">
+      Hello world!
+    </h1>
+     
+    </>
+  )
+}
+
+export default App
